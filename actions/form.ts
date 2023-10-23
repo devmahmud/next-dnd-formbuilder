3a513899a1b2e706b0c6f596795a12da21fab46c
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-23 17:41:28 +0600

    feat: form server actions

---
+++ actions/form.ts
@@ -0,0 +1,78 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { formSchema, formSchemaType } from "@/schemas/form";
+import { currentUser } from "@clerk/nextjs";
+
+class UserNotFoundErr extends Error {}
+
+export async function GetFormStats() {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new UserNotFoundErr("User not found");
+  }
+
+  const stats = await prisma.form.aggregate({
+    where: {
+      userId: user.id,
+    },
+    _sum: {
+      visits: true,
+      submissions: true,
+    },
+  });
+
+  const visits = stats._sum.visits || 0;
+  const submissions = stats._sum.submissions || 0;
+
+  const submissionRate = visits > 0 ? (submissions / visits) * 100 : 0;
+  const bounceRate = 100 - submissionRate;
+
+  return { visits, submissions, submissionRate, bounceRate };
+}
+
+export async function CreateForm(data: formSchemaType) {
+  const validation = formSchema.safeParse(data);
+
+  if (!validation.success) {
+    throw new Error("Invalid form data");
+  }
+
+  const user = await currentUser();
+
+  if (!user) {
+    throw new UserNotFoundErr("User not found");
+  }
+
+  const form = await prisma.form.create({
+    data: {
+      name: data.name,
+      description: data.description,
+      userId: user.id,
+    },
+  });
+
+  if (!form) {
+    throw new Error("Failed to create form");
+  }
+
+  return form.id;
+}
+
+export async function GetForms() {
+  const user = await currentUser();
+
+  if (!user) {
+    throw new UserNotFoundErr("User not found");
+  }
+
+  return await prisma.form.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+}
