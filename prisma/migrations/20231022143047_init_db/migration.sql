bd929fe0683d2def4ba9b2e0a4f30bc65f707c00
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-23 13:42:57 +0600

    feat: prisma model

---
+++ prisma/migrations/20231022143047_init_db/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "Form" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT '',
+    "visits" INTEGER NOT NULL DEFAULT 0,
+    "submissions" INTEGER NOT NULL DEFAULT 0,
+    "shareURL" TEXT NOT NULL,
+
+    CONSTRAINT "Form_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FormSubmissions" (
+    "id" SERIAL NOT NULL,
+    "formId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "FormSubmissions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "FormSubmissions" ADD CONSTRAINT "FormSubmissions_formId_fkey" FOREIGN KEY ("formId") REFERENCES "Form"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
