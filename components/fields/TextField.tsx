05aab028f4d6204921a3f40c3244badbabf7acf0
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-24 22:36:40 +0600

    feat: text field

---
+++ components/fields/TextField.tsx
@@ -0,0 +1,58 @@
+import { Type } from "lucide-react";
+import {
+  ElementsType,
+  FormElement,
+  FormElementInstance,
+} from "../FormElements";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+const type: ElementsType = "TextField";
+const extraAttributes = {
+  label: "Text field",
+  helperText: "Helper text",
+  required: false,
+  placeHolder: "Value here...",
+};
+
+export const TextFieldFormElement: FormElement = {
+  type,
+  construct: (id: string) => ({
+    id,
+    type,
+    extraAttributes,
+  }),
+  designerBtnElement: {
+    icon: Type,
+    label: "Text field",
+  },
+  designerComponent: DesignerComponent,
+  formComponent: () => <div>Form component</div>,
+  propertiesComponent: () => <div>Properties component</div>,
+};
+
+type CustomInstance = FormElementInstance & {
+  extraAttributes: typeof extraAttributes;
+};
+
+function DesignerComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { label, required, placeHolder, helperText } = element.extraAttributes;
+
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <Label>
+        {label}
+        {required && "*"}
+      </Label>
+      <Input readOnly disabled placeholder={placeHolder} />
+      {helperText && (
+        <p className="text-[0.8rem] text-muted-foreground">{helperText}</p>
+      )}
+    </div>
+  );
+}
