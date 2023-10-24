1a3cb7d49c6f449a11f525102641bc2cf28c8205
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-24 22:36:25 +0600

    feat: form elements

---
+++ components/FormElements.tsx
@@ -0,0 +1,29 @@
+import { TextFieldFormElement } from "./fields/TextField";
+
+export type ElementsType = "TextField";
+
+export type FormElement = {
+  type: ElementsType;
+  construct: (id: string) => FormElementInstance;
+  designerBtnElement: {
+    icon: React.ElementType;
+    label: string;
+  };
+  designerComponent: React.FC<{ elementInstance: FormElementInstance }>;
+  formComponent: React.FC;
+  propertiesComponent: React.FC;
+};
+
+export type FormElementInstance = {
+  id: string;
+  type: ElementsType;
+  extraAttributes?: Record<string, any>;
+};
+
+type FormElementsType = {
+  [key in ElementsType]: FormElement;
+};
+
+export const FormElements: FormElementsType = {
+  TextField: TextFieldFormElement,
+};
