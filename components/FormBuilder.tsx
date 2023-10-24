5ced6348a40a789355204d68331196a41a85ed77
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-24 22:36:16 +0600

    feat: form builder

---
+++ components/FormBuilder.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Form } from "@prisma/client";
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import PreviewDialogBtn from "./PreviewDialogBtn";
+import SaveFormBtn from "./SaveFormBtn";
+import PublishFormBtn from "./PublishFormBtn";
+import Designer from "./Designer";
+import DragOverlayWrapper from "./DragOverlayWrapper";
+
+interface Props {
+  form: Form;
+}
+
+function FormBuilder({ form }: Props) {
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10, // 10px
+    },
+  });
+
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 300,
+      tolerance: 5,
+    },
+  });
+
+  const sensors = useSensors(mouseSensor, touchSensor);
+
+  return (
+    <DndContext sensors={sensors}>
+      <main className="flex w-full flex-col">
+        <nav className="flex items-center justify-between gap-3 border-b-2 p-4">
+          <h2 className="truncate font-medium">
+            <span className="mr-2 text-muted-foreground">Form:</span>
+            {form.name}
+          </h2>
+          <div className="flex items-center gap-2">
+            <PreviewDialogBtn />
+            {!form.published && (
+              <>
+                <SaveFormBtn />
+                <PublishFormBtn />
+              </>
+            )}
+          </div>
+        </nav>
+        <div className="relative flex h-[200px] w-full flex-grow items-center justify-center overflow-y-auto bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+          <Designer />
+        </div>
+      </main>
+      <DragOverlayWrapper />
+    </DndContext>
+  );
+}
+
+export default FormBuilder;
