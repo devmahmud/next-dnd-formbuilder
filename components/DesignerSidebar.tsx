4ae3944e3792f0dcd536a875e29f12da2a8ff391
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-25 13:44:41 +0600

    feat(UI): sidebar properties

--- components/DesignerSidebar.tsx
+++ components/DesignerSidebar.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { FormElements } from "./FormElements";
-import SidebarBtnElement from "./SidebarBtnElement";
+import useDesigner from "./hooks/useDesigner";
+import FormElementsSidebar from "./FormElementsSidebar";
+import PropertiesFormSidebar from "./PropertiesFormSidebar";
 
 function DesignerSidebar() {
+  const { selectedElement } = useDesigner();
+
   return (
     <aside className="flex h-full w-[400px] max-w-[400px] flex-grow flex-col gap-2 overflow-y-auto border-l-2 border-muted bg-background p-4">
-      Elements
-      <SidebarBtnElement formElement={FormElements.TextField} />
+      {!selectedElement && <FormElementsSidebar />}
+      {selectedElement && <PropertiesFormSidebar />}
     </aside>
   );
 }
