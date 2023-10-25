752945e0d552c4120322021f8498bca82e067cce
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-25 13:45:30 +0600

    feat(UI): properties form sidebar

---
+++ components/PropertiesFormSidebar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import useDesigner from "./hooks/useDesigner";
+import { FormElements } from "./FormElements";
+import { X } from "lucide-react";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+
+function PropertiesFormSidebar() {
+  const { selectedElement, setSelectedElement } = useDesigner();
+  if (!selectedElement) return null;
+
+  const PropertiesForm =
+    FormElements[selectedElement?.type].propertiesComponent;
+
+  return (
+    <div className="flex flex-col p-2">
+      <div className="flex items-center justify-between">
+        <p className="text-sm text-foreground/70">Element properties</p>
+        <Button
+          size={"icon"}
+          variant={"ghost"}
+          onClick={() => {
+            setSelectedElement(null);
+          }}
+        >
+          <X />
+        </Button>
+      </div>
+      <Separator className="mb-4" />
+      <PropertiesForm elementInstance={selectedElement} />
+    </div>
+  );
+}
+
+export default PropertiesFormSidebar;
