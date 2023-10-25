bfca2b7b95708a231adb26589b37ee71dec46159
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-25 13:45:41 +0600

    feat(UI): form element sidebar

---
+++ components/FormElementsSidebar.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import SidebarBtnElement from "./SidebarBtnElement";
+import { FormElements } from "./FormElements";
+import { Separator } from "./ui/separator";
+
+function FormElementsSidebar() {
+  return (
+    <div>
+      <p className="text-sm text-foreground/70">Drag and drop elements</p>
+      <Separator className="my-2" />
+      <div className="grid grid-cols-1 place-items-center gap-2 md:grid-cols-2">
+        <p className="col-span-1 my-2 place-self-start text-sm text-muted-foreground md:col-span-2">
+          Layout elements
+        </p>
+        {/* <SidebarBtnElement formElement={FormElements.TitleField} />
+        <SidebarBtnElement formElement={FormElements.SubTitleField} />
+        <SidebarBtnElement formElement={FormElements.ParagraphField} />
+        <SidebarBtnElement formElement={FormElements.SeparatorField} />
+        <SidebarBtnElement formElement={FormElements.SpacerField} /> */}
+
+        <p className="col-span-1 my-2 place-self-start text-sm text-muted-foreground md:col-span-2">
+          Form elements
+        </p>
+        <SidebarBtnElement formElement={FormElements.TextField} />
+        {/* <SidebarBtnElement formElement={FormElements.NumberField} />
+        <SidebarBtnElement formElement={FormElements.TextAreaField} />
+        <SidebarBtnElement formElement={FormElements.DateField} />
+        <SidebarBtnElement formElement={FormElements.SelectField} />
+        <SidebarBtnElement formElement={FormElements.CheckboxField} /> */}
+      </div>
+    </div>
+  );
+}
+
+export default FormElementsSidebar;
