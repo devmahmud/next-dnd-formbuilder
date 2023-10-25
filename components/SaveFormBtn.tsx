a1d1d68f4c5a95ca2d3f39b2de0e1de16ffe5f2a
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-25 15:22:38 +0600

    feat: form save to db

--- components/SaveFormBtn.tsx
+++ components/SaveFormBtn.tsx
@@ -1,11 +1,43 @@
-import { SaveAll } from "lucide-react";
+import { Loader2, SaveAll } from "lucide-react";
 import { Button } from "./ui/button";
+import { toast } from "./ui/use-toast";
+import { UpdateFormContent } from "@/actions/form";
+import { useTransition } from "react";
+import useDesigner from "./hooks/useDesigner";
 
-function SaveFormBtn() {
+function SaveFormBtn({ id }: { id: number }) {
+  const { elements } = useDesigner();
+  const [loading, startTransition] = useTransition();
+
+  const updateFormContent = async () => {
+    try {
+      const jsonElements = JSON.stringify(elements);
+      await UpdateFormContent(id, jsonElements);
+      toast({
+        title: "Success",
+        description: "Your form has been saved!",
+      });
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Something went wrong!",
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
-    <Button variant="outline" className="gap-2">
+    <Button
+      variant="outline"
+      className="gap-2"
+      disabled={loading}
+      onClick={() => {
+        startTransition(updateFormContent);
+      }}
+    >
       <SaveAll className="h-6 w-6" />
       Save
+      {loading && <Loader2 className="animate-spin" />}
     </Button>
   );
 }
