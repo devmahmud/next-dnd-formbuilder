8bb57b2afe0ec12b5c7f5e668404eb35cfcd3af5
Mahmudul Alam <expelmahmud@gmail.com>
2023-10-30 11:32:17 +0600

    feat: subtitle field

---
+++ components/fields/SubTitleField.tsx
@@ -0,0 +1,141 @@
+import { Heading2 } from "lucide-react";
+import * as z from "zod";
+import {
+  ElementsType,
+  FormElement,
+  FormElementInstance,
+} from "../FormElements";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import useDesigner from "../hooks/useDesigner";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+
+const type: ElementsType = "SubTitleField";
+const extraAttributes = {
+  title: "SubTitle field",
+};
+
+const propertiesSchema = z.object({
+  title: z.string().min(2).max(100),
+});
+
+export const SubTitleFieldFormElement: FormElement = {
+  type,
+  construct: (id: string) => ({
+    id,
+    type,
+    extraAttributes,
+  }),
+  designerBtnElement: {
+    icon: Heading2,
+    label: "SubTitle field",
+  },
+  designerComponent: DesignerComponent,
+  formComponent: FormComponent,
+  propertiesComponent: PropertiesComponent,
+  validate: () => true,
+};
+
+type CustomInstance = FormElementInstance & {
+  extraAttributes: typeof extraAttributes;
+};
+
+type PropertiesFormSchemaType = z.infer<typeof propertiesSchema>;
+
+function DesignerComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { title } = element.extraAttributes;
+
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <Label className="text-muted-foreground">SubTitle field</Label>
+      <p className="text-lg">{title}</p>
+    </div>
+  );
+}
+
+function FormComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { title } = element.extraAttributes;
+
+  return <p className="text-lg">{title}</p>;
+}
+
+function PropertiesComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const { updateElement } = useDesigner();
+  const element = elementInstance as CustomInstance;
+  const form = useForm<PropertiesFormSchemaType>({
+    resolver: zodResolver(propertiesSchema),
+    mode: "onBlur",
+    defaultValues: {
+      title: element.extraAttributes.title,
+    },
+  });
+
+  useEffect(() => {
+    form.reset(element.extraAttributes);
+  }, [element, form]);
+
+  function applyChanges(values: PropertiesFormSchemaType) {
+    const { title } = values;
+    updateElement(element.id, {
+      ...element,
+      extraAttributes: {
+        title,
+      },
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        onBlur={form.handleSubmit(applyChanges)}
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel htmlFor={field.name}>Title</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      e.currentTarget.blur();
+                    }
+                  }}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
